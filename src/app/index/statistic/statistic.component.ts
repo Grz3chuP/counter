0f1363969e378fc4fff721b6cf6891bc2ab1826d
grzejatol <grzejatol@gmail.com>
2023-11-10 23:16:32 +0000

    statystyki wstepnie dzialaja przed update angulara

--- src/app/index/statistic/statistic.component.ts
+++ src/app/index/statistic/statistic.component.ts
@@ -16,12 +16,17 @@ export class StatisticComponent implements OnInit{
   dateFrom: any;
   dateTo: any;
   uniqObjectsName: string[] = [];
+  uniqName: string = '';
   newObjectsList: ObjectInterface[] = [];
+  newObjectNameList: ObjectInterface[] = [];
+  weekdays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 constructor(private addItemService: AddItemsService) {
 }
  ngOnInit(): void {
-  // this.dateFrom = DateTime.now().toISO()
-  // this.dateTo = DateTime.now().toISO()
+   this.dateFrom = DateTime.now().toLocaleString();
+   console.log(this.dateFrom);
+   this.dateTo = DateTime.now().toISO();
+   console.log(this.dateTo);
 }
 // getUniqObjectsName() {
 //   const uniqObjectsName: string[] = [];
@@ -80,13 +85,29 @@ constructor(private addItemService: AddItemsService) {
   //computed sprawdza czy obiekt jest nowy czy zmieniony
 
   checkIfNewObjectChanged =() => {
-    let test: string[] = [];
+    let uniqName: string[] = [];
      // this.uniqObjectsName = [...new Set(this.newObjectsList.map((object) => object.object_name))]
-    test = [...new Set(this.newObjectsList.map((object) => object.object_name))]
-    console.log(test);
-    return test;
+    uniqName = [...new Set(this.newObjectsList.map((object) => object.object_name))]
+    console.log(uniqName);
+    this.uniqObjectsName = uniqName;
+    return uniqName;
 
   };
+  getEventListForDay = () => {
+    this.newObjectNameList = this.newObjectsList.filter((object) => object.object_name.includes(this.uniqName));
+  }
     protected readonly categoryList = categoryList;
+  getDayOfWeekList(day: number) {
+    const dayList = this.newObjectNameList.filter(object =>    {
+      const createdAtDate =  DateTime.fromISO(object.created_at)
+      return createdAtDate.weekday  === day + 1 });
+    return dayList;
+  }
 
+  getDayTotalValue(day: number) {
+    const dayList = this.getDayOfWeekList(day);
+    const dayTotalValue = dayList.reduce((sum, object) => sum + object.value, 0);
+    return dayTotalValue;
+  }
+  protected readonly event = event;
 }
